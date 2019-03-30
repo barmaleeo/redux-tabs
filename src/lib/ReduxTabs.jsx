24fd931b2efe7bb9f5673a784665216113b2c343
pavel <s20305382@gmail.com>
2019-03-30 20:04:50 +0200

    Check children bounds

--- src/lib/ReduxTabs.jsx
+++ src/lib/ReduxTabs.jsx
@@ -13,13 +13,13 @@ export default class ReduxTabs extends Component {
         return (
             <div className="redux-tabs-outher">
                 <ul className="nav nav-tabs">
-                    {p.children.map((c,n) => (
+                    {p && p.children && p.children.map((c,n) => (
                         <li key={n} className={parseInt(p.active)===n?'active':null}>
                             <a href="/#" onClick={this.handleClickTab.bind(this, n)}>{c.props.name}</a>
                         </li>))}
                 </ul>
                 <div {...p.contentProps}>
-                    {p.children[p.active]}
+                    {(p.children && p.children.length>p.active)?p.children[p.active]:null}
                 </div>
             </div>
         )
